86a73e02f301b2af01decfcd4d395015a080cb32
Irina Egorova <48714516+eiririna@users.noreply.github.com>
2022-09-16 20:46:56 +0300

    Update 20220903192440_create_articles.rb

--- db/migrate/20220903192440_create_articles.rb
+++ db/migrate/20220903192440_create_articles.rb
@@ -1,11 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
-    create_table :request_responses do |t|
-      t.string :remote_ip, null:false
-      t.string :request_method, null:false
-      t.string :request_url, null:false
-      t.string :response_status, null:false
-      t.string :response_content_type, null:false
-    end
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
   end
 end
